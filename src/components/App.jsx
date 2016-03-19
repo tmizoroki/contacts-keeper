6276f669126a86730766928e59314786c57a2b13
Tomio Mizoroki <tmizoroki@gmail.com>
2016-03-19 16:20:34 -0700

    Import FiterableContactTable and create set state to dummy contact info

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,12 +1,33 @@
 import React from 'react';
+import FilterableContactsTable from './FilterableContactsTable.jsx'
 
 class App extends React.Component {
     constructor() {
-        super();
-        this.displayName = 'App';
+      super();
+      this.state = {
+        contacts: [
+          {
+            firstName: 'Tomio',
+            lastName: 'Mizoroki',
+            dob: 'May 31 1986',
+            phone: '415-302-7561',
+            email: 'tmizoroki@gmail.com',
+            notes: 'He\'s looking for work as a software developer'
+          },
+          {
+            firstName: 'Victoria',
+            lastName: 'Wolseley',
+            dob: 'January 25 1987',
+            phone: '805-555-5555',
+            email: 'victoriawolseley@gmail.com',
+            notes: 'Trying to get into a MD/PHd program'
+          },
+        ]
+      }
     }
+
     render() {
-        return <div>App</div>;
+        return <FilterableContactsTable contacts={this.state.contacts} />;
     }
 }
 
