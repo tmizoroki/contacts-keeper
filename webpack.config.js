a1b05859b3ab15aa1d653134a42d1fcd19fef439
Tomio Mizoroki <tmizoroki@gmail.com>
2016-03-19 03:05:52 -0700

    Add babel-loader to loaders in webpack config file

--- webpack.config.js
+++ webpack.config.js
@@ -2,5 +2,14 @@ module.exports = {
   entry: "./src/app.js",
   output: {
     filename: "bundle.js"
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader'
+      }
+    ]
   }
 }
