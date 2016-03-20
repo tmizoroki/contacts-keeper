350cf1202f949ac4e0d6a3d239a9b03b801daecb
Tomio Mizoroki <tmizoroki@gmail.com>
2016-03-20 15:25:41 -0700

    Set up testing environment

--- webpack.config.js
+++ webpack.config.js
@@ -11,5 +11,11 @@ module.exports = {
         loader: 'babel-loader'
       }
     ]
+  },
+  externals: {
+    'cheerio': 'window',
+    'react-dom': true,
+    'react-dom/server': true,
+    'react-addons-test-utils': true
   }
 }
