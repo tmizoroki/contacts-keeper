ac92a81f8cc435ee0a902c5e54d9fd776b839c5a
Tomio Mizoroki <tmizoroki@gmail.com>
2016-03-20 16:09:32 -0700

    Modify contact table test to make it more flexible

--- test/componentSpecs/ContactTableSpec.js
+++ test/componentSpecs/ContactTableSpec.js
@@ -6,33 +6,23 @@ import ContactRow from '../../src/components/ContactRow.jsx';
 
 let expect = chai.expect;
 
-function mockRows() {
-  return [
-    {
-      firstName: 'Tomio',
-      lastName: 'Mizoroki',
-      dob: 'May 31 1986',
-      phone: '415-302-7561',
-      email: 'tmizoroki@gmail.com',
-      notes: 'this is a note'
-    },
-    {
-      firstName: 'Tomio',
-      lastName: 'Mizoroki',
-      dob: 'May 31 1986',
-      phone: '415-302-7561',
-      email: 'tmizoroki@gmail.com',
-      notes: 'this is a note'
-    }
-  ]
-}
+let mockContact = () => {
+  return {
+    firstName: 'Tomio',
+    lastName: 'Mizoroki',
+    dob: 'May 31 1986',
+    phone: '415-302-7561',
+    email: 'tmizoroki@gmail.com',
+    notes: 'this is a note'
+  };
+};
 
 describe('<ContactTable />', () => {
 
   it('renders a table with two contacts', () => {
-    const rows = mockRows();
+    const rows = [mockContact(), mockContact()];
     const wrapper = shallow(<ContactTable contacts={rows} />);
-    expect(wrapper.find(ContactRow)).to.have.length(2);
+    expect(wrapper.find(ContactRow)).to.have.length(rows.length);
   });
 
 });
