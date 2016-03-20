b0a6f700ec2767cba8335db2aac73a55e5eba18c
Tomio Mizoroki <tmizoroki@gmail.com>
2016-03-20 15:21:29 -0700

    Destructure contact props and use those variables inside of the JSX

--- src/components/ContactRow.jsx
+++ src/components/ContactRow.jsx
@@ -1,15 +1,17 @@
 import React from 'react';
 
-const ContactRow = (props) =>
-  (
+const ContactRow = (props) => {
+  const {firstName, lastName, dob, phone, email, notes} = props.contact;
+  return (
     <tr>
-      <td>{props.firstName}</td>
-      <td>{props.lastName}</td>
-      <td>{props.dob}</td>
-      <td>{props.phone}</td>
-      <td>{props.email}</td>
-      <td>{props.notes}</td>
+      <td className="firstName">{firstName}</td>
+      <td className="lastName">{lastName}</td>
+      <td className="dob">{dob}</td>
+      <td className="phone">{phone}</td>
+      <td className="email">{email}</td>
+      <td className="notes">{notes}</td>
     </tr>
   );
+}
 
 export default ContactRow;
