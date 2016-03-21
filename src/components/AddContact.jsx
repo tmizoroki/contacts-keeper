d0ecbf683f2500c298c43275b9adecdf4dafdc1a
Tomio Mizoroki <tmizoroki@gmail.com>
2016-03-20 19:32:56 -0700

    Convert from stateless functional button component to a contained modal that itself contains a form.

--- src/components/AddContact.jsx
+++ src/components/AddContact.jsx
@@ -1,6 +1,52 @@
 import React from 'react';
+import { Button, Modal, Input, ButtonInput } from 'react-bootstrap';
 
-const AddContact = (props) =>
-  <button type="Button">Contacts Keeper</button>
+class AddContact extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = { show: false };
+    }
 
-export default AddContact
+    render() {
+        let close = () => this.setState({ show:false });
+        let save;
+
+        return (
+          <div className="modal-container" style={{height: 200}}>
+            <Button
+              bsStyle="primary"
+              bsSize="large"
+              onClick={() => this.setState({ show: true})}
+            >
+              Contacts Keeper
+            </Button>
+
+            <Modal
+              show={this.state.show}
+              onHide={close}
+              container={this}
+              aria-labelledby="contained-modal-title"
+            >
+              <Modal.Header closeButton>
+                <Modal.Title id="contained-modal-title">Contacts Keeper</Modal.Title>
+              </Modal.Header>
+              <Modal.Body>
+                <form>
+                  <Input type="text" label="First Name" placeholder="" />
+                  <Input type="text" label="Last Name" placeholder="" />
+                  <Input type="date" label="Date of Birth" placeholder="" />
+                  <Input type="tel" label="Phone Number" placeholder="" />
+                  <Input type="email" label="Email" placeholder="" />
+                  <Input type="textarea" label="Notes" placeholder="" />
+                </form>
+              </Modal.Body>
+              <Modal.Footer>
+                <Button onClick={save}>Save</Button>
+              </Modal.Footer>
+            </Modal>
+          </div>
+        );
+    }
+}
+
+export default AddContact;
