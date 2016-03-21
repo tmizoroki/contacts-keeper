0a693a81098bb5d6145bfaf20547eef67620bb01
Tomio Mizoroki <tmizoroki@gmail.com>
2016-03-21 14:45:49 -0700

    Bind the search bar value to the state in FilterableContactsTable. Add proptypes.

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,14 +1,28 @@
-import React from 'react';
+import React, { PropTypes } from 'react';
 import { Input, Glyphicon, Button } from 'react-bootstrap';
 
-const SearchBar = (props) =>
-  (
+const SearchBar = ({filterText, onUserInput}) => {
+
+  let handleChange = e => onUserInput(e.target.value);
+
+  return (
     <form className="input-group">
-      <Input type="text" placeholder="Search" />
+      <Input
+        type="text"
+        placeholder="Search"
+        value={filterText}
+        onChange={handleChange}
+      />
       <span className="input-group-btn">
         <Button><Glyphicon glyph="search" /></Button>
       </span>
     </form>
   )
+}
+
+SearchBar.propTypes = {
+  filterText: PropTypes.string,
+  onUserInput: PropTypes.func.isRequired
+}
 
 export default SearchBar;
