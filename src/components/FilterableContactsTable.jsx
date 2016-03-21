892cb40d86228815a8ca832f6374e9866ff853a3
Tomio Mizoroki <tmizoroki@gmail.com>
2016-03-21 14:49:07 -0700

    Write callback functions that will bind the search bar values and the state of the AddContact component to FilterableContactsTable's state. Layout the component using react-bootstrap Grids and Cols

--- src/components/FilterableContactsTable.jsx
+++ src/components/FilterableContactsTable.jsx
@@ -4,28 +4,47 @@ import SearchBar from './SearchBar.jsx';
 import AddContact from './AddContact.jsx';
 import ContactTable from './ContactTable.jsx';
 
-const FilterableContactsTable = ({contacts}) => {
-  return (
-    <Grid>
-      <Row className='show-grid'>
-        <Col md={4}>
-          <SearchBar />
-        </Col>
-        <Col md={4} mdPush={6}>
-          <AddContact />
-        </Col>
-      </Row>
-      <Row className='show-grid'>
-        <Col md={12}>
-          <ContactTable contacts={contacts} />
-        </Col>
-      </Row>
-    </Grid>
-  );
-}
+class FilterableContactsTable extends React.Component {
+  
+  constructor(props) {
+    super(props);
+    this.state = {
+      filterText: '',
+      contacts: []
+    }
+  }
+  
+  handleUserInput(filterText) { 
+    this.setState({
+      filterText
+    });
+  };
 
-FilterableContactsTable.propTypes = {
-    contacts: PropTypes.array.isRequired
+  handleContact(contactForm) {
+    this.setState({
+      contacts: this.state.contacts.concat(contactForm)
+    });
+  };
+
+  render() {
+    return (
+      <Grid>
+        <Row className='show-grid'>
+          <Col md={4}>
+            <SearchBar filterText={this.state.filterText} onUserInput={this.handleUserInput.bind(this)} />
+          </Col>
+          <Col md={4} mdPush={6}>
+            <AddContact onFormSubmit={this.handleContact.bind(this)} />
+          </Col>
+        </Row>
+        <Row className='show-grid'>
+          <Col md={12}>
+            <ContactTable contacts={this.state.contacts} filterText={this.state.filterText} />
+          </Col>
+        </Row>
+      </Grid>
+    );
+  }
 }
 
 export default FilterableContactsTable;
