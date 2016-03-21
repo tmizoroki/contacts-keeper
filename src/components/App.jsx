738658875b0cf820b12749a8fbd8657411cc0926
Tomio Mizoroki <tmizoroki@gmail.com>
2016-03-21 14:55:06 -0700

    Convert App.jsx back into a stateless functional component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,49 +2,21 @@ import React from 'react';
 import { Navbar } from 'react-bootstrap';
 import FilterableContactsTable from './FilterableContactsTable.jsx'
 
-class App extends React.Component {
-    constructor() {
-      super();
-      this.state = {
-        contacts: [
-          {
-            firstName: 'Tomio',
-            lastName: 'Mizoroki',
-            dob: 'May 31 1986',
-            phone: '415-302-7561',
-            email: 'tmizoroki@gmail.com',
-            notes: 'He\'s looking for work as a software developer'
-          },
-          {
-            firstName: 'Victoria',
-            lastName: 'Wolseley',
-            dob: 'January 25 1987',
-            phone: '805-555-5555',
-            email: 'victoriawolseley@gmail.com',
-            notes: 'Trying to get into a MD/PHd program'
-          },
-        ]
-      }
-    }
+const App = () =>
+  (
+    <div>
+      <Navbar>
+        <Navbar.Header>
+          <Navbar.Brand>
+            Contacts Keeper
+          </Navbar.Brand>
+        </Navbar.Header>
+      </Navbar>
 
-    render() {
-      return (
-        <div>
-          <Navbar>
-            <Navbar.Header>
-              <Navbar.Brand>
-                Contacts Keeper
-              </Navbar.Brand>
-            </Navbar.Header>
-          </Navbar>
-
-          <div className='container'>
-            <FilterableContactsTable contacts={this.state.contacts} />
-          </div>
-        </div>
-      )
-
-    }
-}
+      <div className='container'>
+        <FilterableContactsTable />
+      </div>
+    </div>
+  )
 
 export default App;
