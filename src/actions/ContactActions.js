f24da229443ba60bda29186bc2adadd3a7ee16bb
Tomio Mizoroki <tmizoroki@gmail.com>
2016-03-21 21:06:09 -0700

    Write actions for addContact and filterContacts

---
+++ src/actions/ContactActions.js
@@ -0,0 +1,15 @@
+import * as types from '../constants/ActionTypes';
+
+export function addContact(contact) {
+  return {
+    type: types.ADD_CONTACT,
+    contact
+  };
+}
+
+export function filterContacts(text) {
+  return {
+    type: types.FILTER_CONTACTS,
+    text
+  };
+}
