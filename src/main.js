4ecdebd95dc8376b9e706a61d512f1a7a82d2685
Tomio Mizoroki <tmizoroki@gmail.com>
2016-03-22 21:35:42 -0700

    Fix path to styles

--- src/main.js
+++ src/main.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import configureStore from './store/configureStore';
 import App from './components/App.jsx';
 import '../node_modules/bootstrap/dist/css/bootstrap.css';
-import './styles/styles.css';
+import '../styles/styles.css';
 
 const store = configureStore();
 
