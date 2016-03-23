ae7b93d039c11e18e64b8e595b59032ab19f95fe
Tomio Mizoroki <tmizoroki@gmail.com>
2016-03-22 19:59:57 -0700

    Write test for the filter

--- test/componentSpecs/ContactTableSpec.js
+++ test/componentSpecs/ContactTableSpec.js
@@ -6,7 +6,7 @@ import ContactRow from '../../src/components/ContactRow.jsx';
 
 let expect = chai.expect;
 
-let mockContact = () => {
+let mockContact1 = () => {
   return {
     firstName: 'Tomio',
     lastName: 'Mizoroki',
@@ -17,12 +17,32 @@ let mockContact = () => {
   };
 };
 
+let mockContact2 = () => {
+  return {
+    firstName: 'John',
+    lastName: 'Smith',
+    dob: 'Jan 01 1786',
+    phone: '415-302-7561',
+    email: 'tmizoroki@gmail.com',
+    notes: 'this is a note'
+  }
+}
+
 describe('<ContactTable />', () => {
 
   it('renders a table with two contacts', () => {
-    const rows = [mockContact(), mockContact()];
-    const wrapper = shallow(<ContactTable contacts={rows} />);
+    const rows = [mockContact1(), mockContact1()];
+    const text = '';
+    const wrapper = shallow(<ContactTable contacts={rows} textToFilter={text} />);
     expect(wrapper.find(ContactRow)).to.have.length(rows.length);
   });
+
+  it('filters contacts that whose first or last names don\'t match the textToFilter text', () => {
+    const rows = [mockContact1(), mockContact2()];
+    const text = 'Tom';
+    const wrapper = shallow(<ContactTable contacts={rows} textToFilter={text} />);
+    expect(wrapper.find(ContactRow)).to.have.length(1);
+    expect(wrapper.find(ContactRow).props().contact.firstName).to.equal('Tomio');
+  })
 
 });
