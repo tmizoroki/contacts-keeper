b12967bbed9ed4117df05bdd2e8a33a4b92b72c9
Tomio Mizoroki <tmizoroki@gmail.com>
2016-03-22 22:08:33 -0700

    Include path to bootstrap in node_modues for css loaders include

--- webpack-production.config.js
+++ webpack-production.config.js
@@ -30,7 +30,10 @@ module.exports = {
         include: path.join(__dirname, 'src') },
       { test: /\.css$/,
         loader: 'style!css',
-        include: path.join(__dirname, 'styles') },
+        include: [
+          path.resolve(__dirname, 'node_modules/bootstrap/dist/css/bootstrap.css'),
+          path.resolve(__dirname, 'public/styles')
+        ] },
       { test: /\.eot(\?v=\d+\.\d+\.\d+)?$/, loader: "file" },
       { test: /\.(woff|woff2)$/, loader:"url?prefix=font/&limit=5000" },
       { test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/, loader: "url?limit=10000&mimetype=application/octet-stream" },
