fc12f56931daea025dd3715620d885f35651a764
Tomio Mizoroki <tmizoroki@gmail.com>
2016-03-22 20:00:58 -0700

    Modify css loader to narrow down where it can find css files

--- webpack.config.js
+++ webpack.config.js
@@ -24,7 +24,14 @@ module.exports = {
         exclude: /node_modules/,
         loader: 'babel-loader'
       },
-      { test: /\.css$/, loader: 'style-loader!css-loader' },
+      {
+        test: /\.css$/, 
+        include: [
+          path.resolve(__dirname, 'node_modules/bootstrap/dist/css/bootstrap.css'),
+          path.resolve(__dirname, 'public/styles')
+        ],
+        loader: 'style-loader!css-loader'
+      },
       { test: /\.eot(\?v=\d+\.\d+\.\d+)?$/, loader: "file" },
       { test: /\.(woff|woff2)$/, loader:"url?prefix=font/&limit=5000" },
       { test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/, loader: "url?limit=10000&mimetype=application/octet-stream" },
