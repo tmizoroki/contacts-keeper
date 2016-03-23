70645ad825c5ed62ad861dde6f05cdbdedd647f1
Tomio Mizoroki <tmizoroki@gmail.com>
2016-03-23 03:04:31 -0700

    Remove import boostrap

--- src/main.js
+++ src/main.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import configureStore from './store/configureStore';
 import App from './components/App.jsx';
-import '../node_modules/bootstrap/dist/fonts/*';
 import '../styles/styles.css';
 
 const store = configureStore();
