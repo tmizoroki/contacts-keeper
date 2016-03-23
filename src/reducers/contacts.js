e0601143599e072a50cef41f4017d1502f7cb98d
Tomio Mizoroki <tmizoroki@gmail.com>
2016-03-22 19:55:53 -0700

    Add in another contract entry to the intial state

--- src/reducers/contacts.js
+++ src/reducers/contacts.js
@@ -1,14 +1,25 @@
 import { ADD_CONTACT } from '../constants/ActionTypes';
 
-const initialState = [{
-  firstName: 'Tomio',
-  lastName: 'Mizoroki',
-  dob: '1986 05 31',
-  phone: '415-302-7561',
-  email: 'tmizoroki@gmail.com',
-  notes: 'Currently unemployed software developer',
-  id: 0
-}];
+const initialState = [
+  {
+    firstName: 'Tomio',
+    lastName: 'Mizoroki',
+    dob: '1986-05-31',
+    phone: '415-302-7561',
+    email: 'tmizoroki@gmail.com',
+    notes: 'Currently unemployed software developer',
+    id: 0
+  },
+  {
+    firstName: 'Danny',
+    lastName: 'Phillips',
+    dob: '1988-01-01',
+    phone: '805-555-5555',
+    email: 'dannyp@dannyp.com',
+    notes: 'Web Manager at Procore',
+    id: 1
+  },
+];
 
 export default function contacts(state = initialState, action) {
   switch (action.type) {
